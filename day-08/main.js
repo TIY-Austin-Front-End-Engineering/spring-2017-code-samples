e7ca8c260c0899e093f5f593db26c71c136664bb
Jess Scheuring <jscheuring@gmail.com>
2017-03-08 12:12:52 -0600

    day 8 notes

---
+++ day-08/main.js
@@ -0,0 +1,47 @@
+'a string is a string of characters';
+'it can contain numbers like 123';
+'it can contain other special words like true or false';
+
+"we try to reserve double quotes for CSS and HTML"
+'use single quotes for JS';
+console.log('a string has quotes around it');
+
+1
+2
+100
+-183
+3.829
+Infinity
+-Infinity
+NaN
+
+console.log(4 * 2);
+
+true
+false
+
+undefined
+console.log(undefined);
+
+var firstName;
+firstName = 'Jess';
+
+var mathAnswer = 4 * 2;
+
+console.log(mathAnswer);
+
+var otherName = 'Neal';
+
+otherName === firstName;
+otherName === 'Neal';
+
+var lastName;
+
+console.log(lastName);
+
+console.log(document);
+console.dir(document);
+
+var fancy = document.querySelector('.fancy');
+console.log(fancy);
+console.dir(fancy);
