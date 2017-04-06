0b9256098c12c5957e52c2d73cdc0ead36f90c33
Justin Herrick <justin@lunarcollective.co>
2017-04-06 15:42:05 -0500

    Adding comments to the defaultSettings.

--- day-27/todo-re-thunk/app/scripts/app.js
+++ day-27/todo-re-thunk/app/scripts/app.js
@@ -1,25 +1,39 @@
 import store from './store.js'
 
 export default function app() {
+
+  // To stop us from having to set the headers in _every_ request.
+  // We could use a feature like jQuery's ajaxSend to setup a before handler.
   $.ajaxSend(function (event, xhr, settings) {
       xhr.setRequestHeader("application-id", "B8B5DF55-71A3-E2A2-FF3C-E6060E190B00");
       xhr.setRequestHeader("secret-key", "0248A80D-3DB7-85BB-FF62-B99778C11A00");
   });
 
-  let settings = {
-    url: "foo",
+  // However, we could instead define our own _default_ settings and update it
+  // by using Object.assign when we want to override it on a specific request
+  let defaultAJAXSettings = {
+    url: "https://api.backendless.com/v1/data/Tasks",
+    type: "GET",
     headers: {
-      "application-id", "foo"
+      "application-id": "B8B5DF55-71A3-E2A2-FF3C-E6060E190B00",
+      "secret-key": "0248A80D-3DB7-85BB-FF62-B99778C11A00"
     },
     data: {}
   }
 
-  var updatedSettings = Object.assign({}, settings, {
+  // Now we can use the default settings for most requests and update them for new ones
+  $.ajax(defaultAjaxSettings).then((tasks) => {
+    console.log(tasks);
+  })
+
+  //Or we can override them with Object.assign
+
+  var postSettings = Object.assign({}, defaultAjaxSettings, {
+    type: "POST",
     data: {
-      foo: "bar"
+      
     }
   });
-
 
   const render = () => {
     let state  = store.getState();
